a5a040e2a2ba23023ca1af91354c65fda398676e
yeogh <52830435+yeogh@users.noreply.github.com>
2022-03-03 13:46:23 +0800

    Updated

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ function hideAttemptShowQn() {
   document.getElementById("result").style.visibility = "hidden";
   document.getElementById("level").style.visibility = "hidden";
   document.getElementById("next").style.visibility = "hidden";
+  document.getElementById("submit").style.visibility = "hidden";
   document.getElementById("gamebrief").style.visibility = "hidden";
   document.getElementById("startagain").style.visibility = "visible";
   document.getElementById("question").style.visibility = "visible";
@@ -40,6 +41,7 @@ function hideQnShowAttempt() {
   document.getElementById("next").style.visibility = "visible";
   document.getElementById("startagain").style.visibility = "visible";
   document.getElementById("reset").style.visibility = "visible";
+  document.getElementById("submit").style.visibility = "visible";
 }
 
 //On Windows Load
@@ -55,6 +57,9 @@ function newGame() {
   setTimeout(hideQnShowAttempt, 5000);
   scoreText = 0;
   score.innerText = `Score: ${scoreText}`;
+  time = 4;
+  duration = 1000;
+  countdown.innerText = "(Your turn in 5 secs)";
   timer();
 }
 
@@ -64,45 +69,49 @@ document.querySelector("#startagain").addEventListener("click", newGame);
 function nextChallenge() {
   hideAttemptShowQn();
   setTimeout(hideQnShowAttempt, 5000);
+  time = 4;
+  duration = 1000;
+  countdown.innerText = "(Your turn in 5 secs)";
+  timer();
 }
 
 document.querySelector("#next").addEventListener("click", nextChallenge);
 
 //Countdown
 let countdown = document.getElementById("countdown");
-let time = 5;
-let duration = 0;
+let time = 4;
+let duration = 1000;
 countdown.innerText = "(Your turn in 5 secs)";
 
 function countdownText() {
   countdown.innerText = `(Your turn in ${time} secs)`;
+  time--;
 }
 
 function timer() {
-  for (let t = 0; t < time; t++) {
-    duration += 1000;
-    time -= 1;
+  for (let t = 0; t < 4; t++) {
     setTimeout(countdownText, duration);
+    duration += 1000;
   }
 }
 
 //Challenge
 let colorArr = ["red", "yellow", "blue", "green"];
 let numArr = [1, 2];
-
+let numofsq = 4;
 let randomQnColorArr = [];
 
 function randomColor() {
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < numofsq; i++) {
     let randomColorIndex = Math.floor(Math.random() * colorArr.length);
     randomQnColorArr[i] = document.getElementsByClassName("randomqn")[i];
-    randomQnColorArr[i].innerText = colorArr[randomColorIndex];
+    // randomQnColorArr[i].innerText = colorArr[randomColorIndex];
     randomQnColorArr[i].classList.add(colorArr[randomColorIndex]);
   }
 }
 
 function randomNum() {
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < numofsq; i++) {
     let randomNumIndex = Math.floor(Math.random() * numArr.length);
     randomQnColorArr[i] = document.getElementsByClassName("randomqn")[i];
     randomQnColorArr[i].innerText = numArr[randomNumIndex];
@@ -205,6 +214,9 @@ document.querySelector("#submit").addEventListener("click", compareResultV2);
 function compareResultV2() {
   document.getElementById("question").style.visibility = "visible";
   document.getElementById("reset").style.visibility = "hidden";
+  document.getElementById("option").style.visibility = "hidden";
+  document.getElementById("reset").style.visibility = "hidden";
+  document.getElementById("submit").style.visibility = "hidden";
   for (let k = 1; k < 5; k++) {
     let ansClasses = document.getElementById(`anssq${k}`).classList;
     if (
@@ -227,6 +239,11 @@ function compareResultV2() {
     } else {
       attemptArr.push("incorrect");
     }
+
+    console.log(attemptArr);
+    console.log(attemptArr.every(checkAttempt));
+    console.log(document.getElementById(`qnsquare${k}`).innerText);
+    console.log(document.getElementById(`anssq${k}`).innerText.trim());
   }
 
   function checkAttempt(element) {
@@ -263,6 +280,118 @@ function compareResultV2() {
       "Opps, not exactly right. Try the next challenge. You've got this!";
   }
 }
+//   function compareResultV3() {
+//     document.getElementById("question").style.visibility = "visible";
+//     document.getElementById("reset").style.visibility = "hidden";
+//     document.getElementById("option").style.visibility = "hidden";
+//     document.getElementById("reset").style.visibility = "hidden";
+//     document.getElementById("submit").style.visibility = "hidden";
+//     for (let k = 1; k < 5; k++) {
+//       let ansClasses = document.getElementById(`anssq${k}`).classList;
+//       if (
+//         document.getElementById(`qnsquare${k}`).classList[3] ===
+//           ansClasses.item(ansClasses.length - 1) &&
+//         document.getElementById(`qnsquare${k}`).innerText ===
+//           document.getElementById(`anssq${k}`).innerText.trim()
+//       ) {
+//         attemptArr.push("correct");
+//       } else if (
+//         document.getElementById(`qnsquare${k}`).classList[3] ===
+//         ansClasses.item(ansClasses.length - 1)
+//       ) {
+//         attemptArr.push("correctcol");
+//       } else if (
+//         document.getElementById(`qnsquare${k}`).innerText ===
+//         document.getElementById(`anssq${k}`).innerText.trim()
+//       ) {
+//         attemptArr.push("correctnum");
+//       } else {
+//         attemptArr.push("incorrect");
+//       }
+
+//       console.log(attemptArr);
+//       console.log(attemptArr.every(checkAttempt));
+//       console.log(document.getElementById(`qnsquare${k}`).innerText);
+//       console.log(document.getElementById(`anssq${k}`).innerText.trim());
+//     }
+
+//     function checkAttempt(element) {
+//       return element === "correct";
+//     }
+
+//     function checkAttemptCol(element) {
+//       return element === "correctcol";
+//     }
+
+//     function checkAttemptNum(element) {
+//       return element === "correctnum";
+//     }
+//     // console.log(attemptArr);
+//     // console.log(attemptArr.every(checkAttempt));
+
+//     if (attemptArr.every(checkAttempt) === true) {
+//       attempt.innerText =
+//         "Awesome! You've got perfect memory. Try the next challenge and make it perfect too!";
+//       scoreText += 10;
+//       score.innerText = `Score: ${scoreText}`;
+//     } else if (attemptArr.every(checkAttemptCol) === true) {
+//       attempt.innerText =
+//         "Colour perfect! 5 points for that. Up your number game at the next challenge.";
+//       scoreText += 5;
+//       score.innerText = `Score: ${scoreText}`;
+//     } else if (attemptArr.every(checkAttemptNum) === true) {
+//       attempt.innerText =
+//         "You are good with numbers. 5 points for that. Cant wait to see the colour match for next challenge.";
+//       scoreText += 5;
+//       score.innerText = `Score: ${scoreText}`;
+//     } else {
+//       attempt.innerText =
+//         "Opps, not exactly right. Try the next challenge. You've got this!";
+//     }
+
+//   if (scoreText === 10) {
+//     let newBlue = document.createElement("li");
+//     newBlue.classList.add("colour", "blue");
+//     newBlue.setAttribute("id", "dragblue");
+//     newBlue.setAttribute("draggable", "true");
+//     newBlue.setAttribute("ondragstart", "drag(event)");
+//     newBlue.innerText = "Blue";
+
+//     let newThree = document.createElement("li");
+//     newThree.classList.add("number");
+//     newThree.setAttribute("id", "dragthree");
+//     newThree.setAttribute("draggable", "true");
+//     newThree.setAttribute("ondragstart", "drag(event)");
+//     newThree.innerText = "3";
+
+//     colourList.appendChild(newBlue);
+//     numberList.appendChild(newThree);
+//     colorArr.push("blue");
+//     numArr.push("3");
+//   } else if (scoreText === 20) {
+//     let newGreen = document.createElement("li");
+//     newGreen.classList.add("colour", "green");
+//     newGreen.setAttribute("id", "draggreen");
+//     newGreen.setAttribute("draggable", "true");
+//     newGreen.setAttribute("ondragstart", "drag(event)");
+//     newGreen.innerText = "Green";
+
+//     let newFour = document.createElement("li");
+//     newFour.classList.add("number");
+//     newFour.setAttribute("id", "dragfour");
+//     newFour.setAttribute("draggable", "true");
+//     newFour.setAttribute("ondragstart", "drag(event)");
+//     newFour.innerText = "4";
+
+//     colourList.appendChild(newGreen);
+//     numberList.appendChild(newFour);
+//     colorArr.push("green");
+//     numArr.push("4");
+//   }
+
+//Increase complexity
+let colourList = document.getElementById("colourlist");
+let numberList = document.getElementById("numberlist");
 
 //Tooltip
 var tooltipTriggerList = [].slice.call(
